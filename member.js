c092843812d2031975459bbfc1627b05fcedf198
Douglas Massolari <douglasmassolari@hotmail.com>
2024-10-01 19:22:20 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,8 @@
+function skillsMembers() {
+    return [
+        { name: 'Nikita', skills: ['JavaScript', 'React', 'Node.js'] },
+        { name: 'Misha', skills: ['JavaScript', 'Angular', 'Express'] },
+        { name: 'Pavel', skills: ['JavaScript', 'React', 'Node.js'] },
+        { name: 'Ivan', skills: ['JavaScript', 'React', 'Express'] },
+    ];
+}
